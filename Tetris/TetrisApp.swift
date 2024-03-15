c8ea05d4280d4d331d5d3fed1fe2582102fc3713
David Solis <doze.inept.0n@icloud.com>
2024-03-15 10:42:27 -0700

    Initial Commit

---
+++ Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by David Solis on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
