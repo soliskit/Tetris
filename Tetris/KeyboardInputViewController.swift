fb3a8908480cfa979ca645654ecacaebc0809be9
David Solis <doze.inept.0n@icloud.com>
2024-03-16 13:02:34 -0700

    remapped keys to support iPadOS

--- Tetris/KeyboardInputViewController.swift
+++ Tetris/KeyboardInputViewController.swift
@@ -18,11 +18,11 @@ class KeyboardInputViewController: UIViewController {
     
     override var keyCommands: [UIKeyCommand]? {
         return [
-            UIKeyCommand(action: #selector(moveLeft), input: UIKeyCommand.inputLeftArrow),
-            UIKeyCommand(action: #selector(moveRight), input: UIKeyCommand.inputRightArrow),
-            UIKeyCommand(action: #selector(rotate), input: UIKeyCommand.inputUpArrow),
-            UIKeyCommand(action: #selector(drop), input: UIKeyCommand.inputDownArrow),
-            UIKeyCommand(action: #selector(hold), input: UIKeyCommand.inputEnd)
+            UIKeyCommand(action: #selector(moveLeft), input: "a", modifierFlags: [], discoverabilityTitle: "Move Piece Left"),
+            UIKeyCommand(action: #selector(moveRight), input: "d", modifierFlags: [], discoverabilityTitle: "Move Piece Right"),
+            UIKeyCommand(action: #selector(rotate), input: "w", modifierFlags: [], discoverabilityTitle: "Rotate"),
+            UIKeyCommand(action: #selector(drop), input: "s", modifierFlags: [], discoverabilityTitle: "Drop Piece"),
+            UIKeyCommand(action: #selector(hold), input: "h", modifierFlags: [], discoverabilityTitle: "Hold or Switch Piece")
         ]
     }
     
