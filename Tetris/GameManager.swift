319181e30e3d191a2a3b442279f867404a852086
David Solis <doze.inept.0n@icloud.com>
2024-03-22 15:54:07 -0700

    added comments

--- Tetris/GameManager.swift
+++ Tetris/GameManager.swift
@@ -7,29 +7,46 @@
 
 import SwiftUI
 
+/// Manages the overall game logic, state, and interactions for a Tetris-like game.
 @Observable
 class GameManager {
     // MARK: - Properties
+    /// Stores the highest score achieved across sessions.
     @ObservationIgnored
     @AppStorage("highScore") private var highScore: Int = 0
+    /// Manages input from an external game controller.
     private var gameControllerManager: GameControllerManager?
+    /// The number of rows in the game board.
     private let rows: Int = 20
+    /// The number of columns in the game board.
     private let columns: Int = 10
+    /// The interval at which Tetrominos naturally drop down the game board.
     private var standardDropInterval: TimeInterval = 0.7
+    /// Timer for managing the periodic dropping of Tetrominos.
     private var timer: Timer?
+    /// The current Tetromino being controlled by the player.
     var currentTetromino: Tetromino
+    /// The next Tetromino that will appear after the current one is placed.
     var nextTetromino: Tetromino
+    /// The Tetromino that is being held for later use.
     var heldTetromino: Tetromino?
+    /// Indicates whether the player can hold a Tetromino.
     var canHoldTetromino: Bool = true
+    /// The game board, represented as a 2D array of optional `GameCell`s.
     var gameBoard: [[GameCell?]]
+    /// The current state of the game (e.g., playing, paused, game over).
     var state: GameState = .gameOver
+    /// The current score of the player.
     var score: Int = 0
+    /// The current level of the game, affecting the drop speed.
     var level: Int = 1
+    /// Calculates a very quick drop interval for soft dropping Tetrominos.
     private var quickDropInterval: TimeInterval {
         standardDropInterval * 0.001
     }
     
     // MARK: - Initialization & Deinitialization
+    /// Initializes a new game manager instance, setting up the initial game state.
     init() {
         currentTetromino = TetrominoFactory.generate()
         nextTetromino = TetrominoFactory.generate()
@@ -37,16 +54,19 @@ class GameManager {
         gameControllerManager = GameControllerManager(gameManager: self)
     }
     
+    /// Cleans up any resources or observers when the game manager is deinitialized.
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
     
     // MARK: - Game State Management
+    /// Starts or restarts the game, resetting the game state and timer.
     func startGame() {
         resetGame()
         startGameTimer()
     }
     
+    /// Resets the game to its initial state, ready for a new game to start.
     private func resetGame() {
         state = .playing
         gameBoard = Array(repeating: Array(repeating: GameCell(), count: columns), count: rows)
@@ -55,12 +75,14 @@ class GameManager {
         heldTetromino = nil
     }
     
+    /// Ends the current game and stops the game timer.
     private func gameOver() {
         state = .gameOver
         stopGameTimer()
     }
     
     // MARK: - Tetromino Management
+    /// Generates the next Tetromino and updates the game state accordingly.
     private func generateNextTetromino() {
         currentTetromino = nextTetromino
         nextTetromino = TetrominoFactory.generate()
@@ -70,6 +92,8 @@ class GameManager {
         }
     }
     
+    /// Drops the current Tetromino one row down or locks it in place if it cannot move further.
+    /// - Parameter isSoftDropping: Indicates if the Tetromino is being soft dropped for faster descent.
     private func dropTetromino(isSoftDropping: Bool = false) {
         guard state == .playing else { return }
         let newPosition = Position(row: currentTetromino.position.row + 1, column: currentTetromino.position.column)
@@ -87,6 +111,7 @@ class GameManager {
         }
     }
     
+    /// Locks the current Tetromino in place on the game board.
     private func lockTetrominoInPlace() {
         currentTetromino.shape.enumerated().forEach { y, row in
             row.enumerated().forEach { x, block in
@@ -101,6 +126,7 @@ class GameManager {
     }
     
     // MARK: - Board Management
+    /// Clears all fully filled rows from the game board and updates the game state.
     private func clearFullRows() {
         let scores = [1: 100, 2: 300, 3: 500, 4: 800]
         let completedLineIndices = gameBoard.indices.filter { row in
@@ -120,29 +146,40 @@ class GameManager {
         }
     }
     
+    /// Checks if a Tetromino's position is valid within the game board.
+    /// - Parameters:
+    ///   - tetromino: The Tetromino to check.
+    ///   - position: The position to check the Tetromino against.
+    /// - Returns: `true` if the Tetromino fits within the game board at the given position, `false` otherwise.
     private func isValidTetrominoPosition(tetromino: Tetromino, at position: Position) -> Bool {
         let newTetromino = Tetromino(shape: tetromino.shape, color: tetromino.color, position: position, rotations: tetromino.rotations, wallKickData: tetromino.wallKickData)
         return newTetromino.fitsWithin(gameBoard: gameBoard)
     }
     
     // MARK: - Timer Management
+    /// Starts or restarts the game timer based on the current game state and whether a soft drop is happening.
+    /// - Parameter withSoftDrop: If `true`, uses the quick drop interval; otherwise, calculates interval based on the level.
     private func startGameTimer(withSoftDrop: Bool = false) {
         let interval = withSoftDrop ? quickDropInterval : standardDropInterval / Double(level)
         stopGameTimer()
         timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(updateGame), userInfo: nil, repeats: true)
     }
     
+    /// Stops the current game timer, if any.
     private func stopGameTimer() {
         timer?.invalidate()
         timer = nil
     }
     
+    /// Called by the game timer to update the game state, typically by dropping the current Tetromino.
     @objc private func updateGame() {
         guard state != .gameOver else { return }
         dropTetromino()
     }
     
     // MARK: - Gameplay Controls
+    /// Handles player actions, translating them into game actions such as moving or rotating the Tetromino.
+    /// - Parameter action: The player action to handle.
     func handleAction(_ action: PlayerAction) {
         switch action {
             case .moveLeft:
@@ -164,6 +201,8 @@ class GameManager {
         }
     }
     
+    /// Moves the current Tetromino horizontally by a specified amount, if the game state allows.
+    /// - Parameter deltaX: The horizontal movement amount.
     private func moveTetromino(horizontalBy deltaX: Int) {
         guard state == .playing else { return }
         let newPosition = Position(row: currentTetromino.position.row, column: currentTetromino.position.column + CGFloat(deltaX))
@@ -172,6 +211,7 @@ class GameManager {
         }
     }
     
+    /// Holds the current Tetromino, swapping it with a previously held Tetromino if available.
     private func holdTetromino() {
         guard state == .playing, canHoldTetromino else { return }
         let previousPosition = currentTetromino.position
@@ -187,11 +227,13 @@ class GameManager {
         }
     }
     
+    /// Rotates the current Tetromino, if the game state allows.
     private func rotateTetromino() {
         guard state == .playing else { return }
         currentTetromino.rotate(gameBoard: gameBoard)
     }
     
+    /// Toggles the game state between paused and playing.
     private func togglePauseResume() {
         if state == .playing {
             state = .paused
--- Tetris/Model/Tetromino.swift
+++ Tetris/Model/Tetromino.swift
@@ -11,14 +11,25 @@ import SwiftUI
 ///
 /// A Tetromino has a specific shape, color, position on the game board, and can be rotated.
 struct Tetromino: Identifiable {
+    /// A unique identifier for each Tetromino instance.
     let id = UUID()
+    /// The 2D array representing the Tetromino's shape, where `true` indicates a block is present.
     var shape: [[Bool]]
+    /// The color of the Tetromino.
     var color: Color
+    /// The current position of the Tetromino on the game board.
     var position: Position
+    /// An array of shapes representing all rotation states of the Tetromino.
     var rotations: [[[Bool]]]
+    /// The index of the current rotation in the `rotations` array.
     var rotationState: Int = 0
+    /// Data used for wall kicks when rotating near walls or other blocks.
     var wallKickData: [[Position]]
     
+    /// Checks if the Tetromino fits within the given game board without overlapping filled cells.
+    ///
+    /// - Parameter gameBoard: A 2D array representing the game board, where each cell may contain a `GameCell`.
+    /// - Returns: A Boolean value indicating whether the Tetromino fits within the game board.
     func fitsWithin(gameBoard: [[GameCell?]]) -> Bool {
         return !shape.enumerated().contains { rowIndex, row in
             row.enumerated().contains { columnIndex, block in
@@ -33,6 +44,12 @@ struct Tetromino: Identifiable {
         }
     }
     
+    /// Rotates the Tetromino, attempting clockwise rotation first, then counter-clockwise if necessary.
+    ///
+    /// The method first tries to rotate the Tetromino clockwise. If the clockwise rotation doesn't fit,
+    /// it attempts a counter-clockwise rotation. Wall kick data is used to test possible positions for each rotation.
+    ///
+    /// - Parameter gameBoard: A 2D array representing the game board to check for fit during rotation.
     mutating func rotate(gameBoard: [[GameCell?]]) {
         let clockwiseRotationState = (rotationState + 1) % rotations.count
         let clockwiseShape = rotations[clockwiseRotationState]
